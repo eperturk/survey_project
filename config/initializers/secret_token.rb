08f1930cc6ef811ddafbb49a37fc57eaf2d03c81
Erol Erturk <erol@ubicare.com>
2013-11-13 19:37:15 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveyProject::Application.config.secret_token = 'c902e793b9be1fab0da424c931267ddccce40ebb8234973a0e2f9d074ae188bd44ef0393bc1131303e8d0ef0ca0b61d02e8bc90d4e49e69ece534bf62aadaaa8'
